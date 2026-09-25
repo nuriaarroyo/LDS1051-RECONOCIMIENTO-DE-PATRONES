093b5dc07fa93b40c2d9d6bb62751a6ad3bb27d8
nuriaarroyo <nuria.arroyobe@udlap.mx>
2026-09-24 20:32:44 -0600

    Agrega ejercicio del primer examen

---
+++ examen1.py
@@ -0,0 +1,19 @@
+#%%
+import numpy
+
+x1 = numpy.array([1, 2, 3])
+x2 = numpy.array([2, 2, 1])
+x3 = numpy.array([-1,-2,2])
+x4 = numpy.array([-3,-3,1])
+x5 = numpy.array([-2, -1, 3])
+x6 = numpy.array([1, -2, -2])
+
+dict = {
+    "c1": [ x1, x2],
+    "c2": [ x3, x4, x5],
+    "c3": [  x6 ]
+}
+
+
+
+# %%
